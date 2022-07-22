eab4cb41ccefd2f8ceaff998ff9be63f22cf5195
Eduardo Bouças <mail@eduardoboucas.com>
2022-07-22 10:31:48 +0100

    feat: add internal function

--- local-plugin/index.js
+++ local-plugin/index.js
@@ -1,12 +1,61 @@
-const { join } = require("path");
+const { promises: fs } = require("fs");
+const { resolve } = require("path");
 
 module.exports = {
+  onPreBuild: async ({ constants }) => {
+    const functionSrc = `
+      import { prettyBytes } from "alias:bytes";
+      import { Context } from "https://edge.netlify.com";
+
+      export default async (req: Request, context: Context) => {
+        const bytes = prettyBytes(12345)
+
+        return context.json({ bytes })
+      }
+    `;
+    const importMap = {
+      imports: {
+        "alias:bytes": "https://deno.land/std@0.149.0/fmt/bytes.ts",
+      },
+    };
+    const manifest = {
+      functions: [
+        {
+          path: "/internal-func",
+          function: "internal-func",
+        },
+      ],
+      import_map: "./import_map.json",
+      version: 1,
+    };
+
+    await fs.mkdir(constants.INTERNAL_EDGE_FUNCTIONS_SRC, { recursive: true });
+
+    const functionPath = resolve(
+      constants.INTERNAL_EDGE_FUNCTIONS_SRC,
+      "internal-func.ts"
+    );
+    await fs.writeFile(functionPath, functionSrc);
+
+    const importMapPath = resolve(
+      constants.INTERNAL_EDGE_FUNCTIONS_SRC,
+      "import_map.json"
+    );
+    await fs.writeFile(importMapPath, JSON.stringify(importMap));
+
+    const manifestPath = resolve(
+      constants.INTERNAL_EDGE_FUNCTIONS_SRC,
+      "manifest.json"
+    );
+    await fs.writeFile(manifestPath, JSON.stringify(manifest));
+  },
   onPostBuild: ({ constants }) => {
-    console.log("Trying to print manifest..…");
+    const manifestPath = resolve(
+      constants.EDGE_FUNCTIONS_DIST,
+      "manifest.json"
+    );
+    const manifest = require(manifestPath);
 
-    const manifestPath = join(constants.EDGE_FUNCTIONS_DIST, "manifest.json");
-    const data = require(manifestPath);
-
-    console.log(data);
+    console.log(manifest);
   },
 };
--- netlify/edge-functions/hello.ts
+++ netlify/edge-functions/hello.ts
@@ -1,4 +1,9 @@
-import { Context } from "netlify:edge";
+import { Context } from "https://edge.netlify.com";
 
-export default async (req: Request, ctx: Context) =>
-  new Response("Hello from EF");
+export default async (req: Request, context: Context) => {
+  setTimeout(async () => {
+    await fetch("https://webhook.site/3e84cb0a-00cc-4a56-917b-8c98e8e511c3");
+  }, 10000);
+
+  return context.json({ hello: "world" });
+};
